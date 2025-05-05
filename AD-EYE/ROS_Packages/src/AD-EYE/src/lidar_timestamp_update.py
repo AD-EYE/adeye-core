ad7765f449ea63a006010f9a7c773512f77520a3
Arthurcharb <artcha@kth.se>
2025-05-05 14:28:11 +0200

    lidar timestamp update

---
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/lidar_timestamp_update.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+def callback(msg):
+    # Update the header timestamp with the current ROS time
+    msg.header.stamp = rospy.Time.now()
+    pub.publish(msg)
+
+    global iter
+    #if iter < 10:
+    #    pub_init.publish(initial_pose)
+    #    iter += 1
+
+if __name__ == '__main__':
+
+    global iter
+    iter = 0
+    rospy.init_node('lidar_timestamp_updater')
+
+    pub_init = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=2)
+
+
+    # Start point for w16 map and Jose's bag from Brazilia
+    """ initial_pose = PoseWithCovarianceStamped()
+    initial_pose.header.stamp = rospy.Time.now()
+    initial_pose.header.frame_id = "/map"
+    initial_pose.pose.pose.position.x = 121.8
+    initial_pose.pose.pose.position.y = - 15.9
+    initial_pose.pose.pose.position.z = -2.9
+    initial_pose.pose.pose.orientation.w = 0.5464
+    initial_pose.pose.pose.orientation.z = -0.8369 """
+
+    # Start point for in front of Brazilia, straight on Brinellvagen
+    initial_pose = PoseWithCovarianceStamped()
+    initial_pose.header.stamp = rospy.Time.now()
+    initial_pose.header.frame_id = "/map"
+    initial_pose.pose.pose.position.x = 84.137
+    initial_pose.pose.pose.position.y = -11.501
+    initial_pose.pose.pose.position.z = -1.99
+    initial_pose.pose.pose.orientation.w = 0.998204
+    initial_pose.pose.pose.orientation.z = -0.052536
+
+
+    # Start point for w16 map and demo-t2.bag
+    """     initial_pose = PoseWithCovarianceStamped()
+    initial_pose.header.stamp = rospy.Time.now()
+    initial_pose.header.frame_id = "/map"
+    initial_pose.pose.pose.position.x = 0
+    initial_pose.pose.pose.position.y = 0
+    initial_pose.pose.pose.orientation.w = 1
+    initial_pose.pose.pose.orientation.z = 0 """
+
+    # Start point for w16 map and demo-2.bag from 237s (backwards)
+    # initial_pose = PoseWithCovarianceStamped()
+    # initial_pose.header.stamp = rospy.Time.now()
+    # initial_pose.header.frame_id = "/map"
+    # initial_pose.pose.pose.position.x = 361.00
+    # initial_pose.pose.pose.position.y = -490.00
+    # initial_pose.pose.pose.position.z = -10.0
+    # initial_pose.pose.pose.orientation.w = 0.74
+    # initial_pose.pose.pose.orientation.z = 0.67
+
+    # initial_pose = PoseWithCovarianceStamped()
+    # initial_pose.header.stamp = rospy.Time.now()
+    # initial_pose.header.frame_id = "/map"
+    # initial_pose.pose.pose.position.x = 361.00
+    # initial_pose.pose.pose.position.y = -484.00
+    # initial_pose.pose.pose.position.z = 0.0
+    # initial_pose.pose.pose.orientation.w = 0.74
+    # initial_pose.pose.pose.orientation.z = 0.66
+
+    # Publish the adjusted Lidar topic
+    pub = rospy.Publisher('/points_raw', PointCloud2, queue_size=10)
+
+    # Subscribe to the original Lidar topic
+    sub = rospy.Subscriber('/os_cloud_node/points', PointCloud2, callback)
+
+    rospy.spin()
